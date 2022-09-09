0df3d4d78fa36dfe0df5be59af6a778223f8e217
Savana Rohit <92589029+savanarohit@users.noreply.github.com>
2022-09-09 10:24:30 +0530

    Update sns_notificaiton_list.py

--- sns/sns_notifications_list.py
+++ sns/sns_notifications_list.py
@@ -5,24 +5,9 @@ import json
 AWS_REGION = "ap-south-1"
 sns = boto3.client('sns', region_name=AWS_REGION)
 
-
-# Function for creating sns topics
-def snscreate():
-    resposne = sns.create_topic(Name='Test3')
-
-
 # Creating multiple SNS Topics
-snscreate()
-
-
-"""
-# List SNS Topics
-response = sns.list_topics()
-for each_sns in response['Topics']:
-    print(each_sns['TopicArn'])
-
-# List SNS Topics with Subscriptions ARN
-response = sns.list_subscriptions()
-for each_subs in response['Subscriptions']:
-    print(each_subs['SubscriptionArn'])
-"""
+snsrange = range(1, 11)
+dict_arn = {}
+for a in snsrange:
+    arn = sns.create_topic(Name=('Test_Topic_')+str(a))
+    dict_arn[a] = arn
