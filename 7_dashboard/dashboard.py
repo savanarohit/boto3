37f1f193f7cbca171aabf48d1768db1c6adcc5e1
Rohit S <srohith9@gmail.com>
2022-09-11 20:26:40 +0530

    update: HTML table created from JSON data

--- 7_dashboard/dashboard.py
+++ 7_dashboard/dashboard.py
@@ -1,6 +1,7 @@
-# import pandas
-import pandas as pd
-pd_object = pd.read_json('astronomy_simple.json', type='series')
-
-df = pd.DataFrame(pd_object)
-print(df)
+from json2table import convert
+json_object = {"key": "value"}
+build_direction = "LEFT_TO_RIGHT"
+table_attributes = {"style": "width:100%"}
+html = convert(json_object, build_direction=build_direction,
+               table_attributes=table_attributes)
+print(html)
