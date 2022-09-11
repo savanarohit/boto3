fefbf5c16a183b33f8e9165c7418c95fda7cc282
Rohit S <srohith9@gmail.com>
2022-09-11 20:14:59 +0530

    code: update

--- 7_dashboard/dashboard.py
+++ 7_dashboard/dashboard.py
@@ -1,28 +1,6 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
+# import pandas
 import pandas as pd
+pd_object = pd.read_json('astronomy_simple.json', type='series')
 
-app = dash.Dash()
-
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/ThuwarakeshM/geting-started-with-plottly-dash/main/life_expectancy.csv"
-)
-
-fig = px.scatter(
-    df,
-    x="GDP",
-    y="Life expectancy",
-    size="Population",
-    color="continent",
-    hover_name="Country",
-    log_x=True,
-    size_max=60,
-)
-
-app.layout = html.Div([dcc.Graph(id="life-exp-vs-gdp", figure=fig)])
-
-
-if __name__ == "__main__":
-    app.run_server(debug=True)
+df = pd.DataFrame(pd_object)
+print(df)
